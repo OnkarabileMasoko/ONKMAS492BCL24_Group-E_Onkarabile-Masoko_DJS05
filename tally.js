0c469b995a87fc18c6b732e6a51b821a327e6766
onkarabile masoko <onkarabilemasoko1@gmail.com>
2024-05-23 15:22:08 +0200

    Consol.loging my code to debug

--- tally.js
+++ tally.js
@@ -38,3 +38,12 @@ class Store {
   // Test Scenarios
   console.log("Initial state:", store.getState()); // Scenario 1
   
+  store.dispatch({ type: "ADD" });
+  store.dispatch({ type: "ADD" }); // Scenario 2
+  
+  store.dispatch({ type: "SUBTRACT" }); // Scenario 3
+  
+  store.dispatch({ type: "RESET" }); // Scenario 4
+  
+  // Unsubscribe
+  unsubscribe();
