c21d8f59f73c4e09f95732bcec40c152393a34c3
onkarabile masoko <onkarabilemasoko1@gmail.com>
2024-05-23 15:23:33 +0200

    Corrected some errors in my code

--- tally.js
+++ tally.js
@@ -1,49 +1,9 @@
-class Store {
-    constructor(reducer, initialState) {
-      this.reducer = reducer;
-      this.state = initialState;
-      this.listeners = [];
-    }
-  
-    getState() {
-      return this.state;
-    }
-  
-    dispatch(action) {
-      this.state = this.reducer(this.state, action);
-      this.listeners.forEach((listener) => listener(this.state));
-    }
-  
-    subscribe(listener) {
-      this.listeners.push(listener);
-      return () => {
-        this.listeners = this.listeners.filter((l) => l !== listener);
-      };
-    }
-  }
-  
-  // Create the Store
-  const store = new Store(tallyReducer, 0);
-  
-  // Subscribe to Changes
-  const unsubscribe = store.subscribe((newState) => {
-    console.log("State updated:", newState);
-  });
+store.dispatch({ type: "ADD" });
+store.dispatch({ type: "ADD" }); // Scenario 2
 
-  // Subscribe to Changes
-  const unsubscribe = store.subscribe((newState) => {
-    console.log("State updated:", newState);
-  });
+store.dispatch({ type: "SUBTRACT" }); // Scenario 3
 
-  // Test Scenarios
-  console.log("Initial state:", store.getState()); // Scenario 1
-  
-  store.dispatch({ type: "ADD" });
-  store.dispatch({ type: "ADD" }); // Scenario 2
-  
-  store.dispatch({ type: "SUBTRACT" }); // Scenario 3
-  
-  store.dispatch({ type: "RESET" }); // Scenario 4
-  
-  // Unsubscribe
+store.dispatch({ type: "RESET" }); // Scenario 4
+
+// Unsubscribe
   unsubscribe();
