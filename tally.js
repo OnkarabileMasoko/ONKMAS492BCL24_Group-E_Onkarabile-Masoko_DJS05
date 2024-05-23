ab6886f3720ed749e6134b5c8c0d8b22aa164fe0
onkarabile masoko <onkarabilemasoko1@gmail.com>
2024-05-23 15:17:03 +0200

    SCENARIO 1: Initial State Verification

---
+++ tally.js
@@ -0,0 +1,24 @@
+class Store {
+    constructor(reducer, initialState) {
+      this.reducer = reducer;
+      this.state = initialState;
+      this.listeners = [];
+    }
+  
+    getState() {
+      return this.state;
+    }
+  
+    dispatch(action) {
+      this.state = this.reducer(this.state, action);
+      this.listeners.forEach((listener) => listener(this.state));
+    }
+  
+    subscribe(listener) {
+      this.listeners.push(listener);
+      return () => {
+        this.listeners = this.listeners.filter((l) => l !== listener);
+      };
+    }
+  }
+  
